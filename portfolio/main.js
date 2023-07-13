7944bbf7a93edc4c2e0a6d928985cdd3e66fd3f1
Vladyslav <5vladik068@gmail.com>
2023-07-13 20:13:33 +0300

    Finished project

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,9 +1,15 @@
+document.querySelector("#scroll-down").addEventListener("click", () => {
+    window.scrollTo({
+        top: document.querySelector("#about-me").offsetTop - 20,
+    });
+});
+
 document.querySelector("#toggle-theme").addEventListener("click", () => {
-  document.documentElement.classList.toggle("light-theme");
+    document.documentElement.classList.toggle("light-theme");
 });
 
 const prefersLight = window.matchMedia("(prefers-color-scheme: light)").matches;
 
 if (prefersLight) {
-  document.documentElement.classList.add("light-theme");
+    document.documentElement.classList.add("light-theme");
 }
